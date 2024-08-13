d3646293036f75836fd821a95f6bcdeb45ddd0ee
slash-arkh <slash-arkh@mail.ru>
2024-08-13 22:30:33 +0300

    Add files via upload

---
+++ module_10_2.py
@@ -0,0 +1,31 @@
+from threading import Thread
+import time
+
+class Knight(Thread):
+    def __init__(self, name, power,*args,**kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = name
+        self.power = power
+        self.army = 100
+        self.day = 0
+
+    def run(self):
+        print(f'{self.name}, на нас напали!')
+        while self.army > 0:
+            self.army -= self.power
+            self.day += 1
+            print(f'{self.name} сражается {self.day} дней..., осталось {self.army} воинов.')
+            time.sleep(1)
+        else:
+            print(f'{self.name} одержал победу спустя {self.day} дней(дня)!')
+
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+
+first_knight.start()
+second_knight.start()
+
+first_knight.join()
+second_knight.join()
+
+print(f'Все кончено, победа за нами!!!')
